1c2e2b9f464c4df010f9c4e2317650df63a1bdc2
TakacsKrisztian <takacsk1@kkszki.hu>
2024-02-22 11:19:22 +0100

    Létrehoztam a projektet.

---
+++ Feladat és SQL fájlok/tantargyak.sql
@@ -0,0 +1,11 @@
+create table tantargyak (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	tantargy_nev VARCHAR(20),
+	tantargy_leiras VARCHAR(50)
+);
+insert into tantargyak (id, tantargy_nev, tantargy_leiras) values (1, 'Matematika', 'Gyakorlat');
+insert into tantargyak (id, tantargy_nev, tantargy_leiras) values (2, 'Magyar', 'Elmélet');
+insert into tantargyak (id, tantargy_nev, tantargy_leiras) values (3, 'Történelem', 'Elmélet');
+insert into tantargyak (id, tantargy_nev, tantargy_leiras) values (4, 'Angol', 'Elmélet');
+insert into tantargyak (id, tantargy_nev, tantargy_leiras) values (5, 'SzakmaiAngol', 'Gyakorlat');
+insert into tantargyak (id, tantargy_nev, tantargy_leiras) values (6, 'Informatika', 'Gyakorlat');
